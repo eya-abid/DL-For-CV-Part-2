eb9cd0c428dd0bb8561630379510cc0b02ff60a0
eya-abid <eyaabid@insat.u-carthage.tn>
2022-05-01 22:01:27 +0100

    preprocessing

--- datasets/dogs_vs_cats/build_dogs_vs_cats.py
+++ datasets/dogs_vs_cats/build_dogs_vs_cats.py
@@ -35,3 +35,41 @@ datasets = [
 # initialize the image preprocessor and the lists of RGB channel averages
 aap = AspectAwarePreprocessor(256,256)
 (R, G, B) = ([], [], [])
+
+# loop over the dataset tuples
+for (dType, paths, labels, outputPath) in datasets:
+    # create HDF5 writer
+    print("[INFO] building {}...".format(outputPath))
+    writer = HDF5DatasetWriter((len(paths),256,256,3), outputPath)
+    
+    # initialize the progress bar
+    widgets = ["Building Dataset: ", progressbar.Percentage(), " ", progressbar.Bar(), " ", progressbar.ETA()]
+    pbar = progressbar.ProgressBar(maxval=len(paths), widgets=widgets).start()
+
+    # loop over the image paths
+    for (i, (path, label)) in enumerate(zip(paths, labels)):
+        # load the image and process it
+        image = cv2.imread(path)
+        image = aap.preprocess(image)
+        
+        # if we are building the training dataset, then compute the mean of each channel in the image, then update the respective lists
+        if dType == "train":
+            (b, g, r) = cv2.mean(image)[:3]
+            R.append(r)
+            G.append(g)
+            B.append(b)
+        
+        # add the image and label # to the HDF5 dataset
+        writer.add([image], [label])
+        pbar.update(i)
+
+        # close the HDF5 writer
+        pbar.finish()
+        writer.close()
+
+# construct a dictionary of averages, then serialize the means to a JSON file
+print("[INFO] serializing means...")
+D = {"R": np.mean(R), "G": np.mean(G), "B": np.mean(B)}
+f = open(config.DATASET_MEAN, "w")
+f.write(json.dumps(D))
+f.close()
